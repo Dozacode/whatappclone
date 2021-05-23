be61276dce5f1061486233fa1dd746fcd71f7033
Doza <gafastdoza@gmail.com>
2021-05-23 00:49:54 +0000

    styled components 2.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
+import ChatsListScreen from './components/ChatsListScreen'
 
-function App() {
-  return (
-    <div>
-      <div>Whatsapp Clone</div>
-      <div>
-        <ul>
-          <li>
-            <img
-              src="https://randomuser.me/api/portraits/thumb/men/1.jpg"
-              alt="Profile"
-            />
-            <div>Ethan Gonzalez</div>
-            <div>You on your way?</div>
-            <div>10:25</div>
-          </li>
-          <li>
-            <img
-              src="https://randomuser.me/api/portraits/thumb/men/2.jpg"
-              alt="Profile"
-            />
-            <div>Bryan Wallace</div>
-            <div>Hey, it's me</div>
-            <div>13:27</div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}
+const App: React.FC = () => (
+
+  <div>
+    <ChatsListScreen/>
+  </div>
+);
+
 
 export default App;
